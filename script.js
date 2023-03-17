481990cb857b52b82064fa4ea886fc522224c720
Aliyah2014 <86959592+Aliyah2014@users.noreply.github.com>
2023-03-17 12:52:49 +0000

    added code to generate password with user input

--- script.js
+++ script.js
@@ -5,37 +5,43 @@ const numericChars = '0123456789';
 const specialChars = '$@%&*()_+-={}[]|/~`';
 
 // Function to prompt user for password options
-function getPasswordOptions() {
+function generatePassword() {
+  // Prompt user for password criteria
   const passwordLength = prompt('How many characters would you like your password to contain? (10-64)');
   const includeLowercase = confirm('Would you like to include lowercase characters?');
   const includeUppercase = confirm('Would you like to include uppercase characters?');
   const includeNumeric = confirm('Would you like to include numeric characters?');
   const includeSpecial = confirm('Would you like to include special characters?');
+
+  // To validate users choice of password
+  if (isNaN(passwordLength) || passwordLength < 10 || passwordLength > 64) {
+    alert('Password length must be a number between 10 and 64!');
+    return '';
+  }
+  if (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecial) {
+    alert('You must select at least one character type!');
+    return '';
+  }
+
+ // variable declaration to generate password with user input
+  let password = '';
+  let charSet = '';
+  if (includeLowercase) charSet += lowercaseChars;
+  if (includeUppercase) charSet += uppercaseChars;
+  if (includeNumeric) charSet += numericChars;
+  if (includeSpecial) charSet += specialChars;
+  for (let i = 0; i < passwordLength; i++) {
+    password += charSet.charAt(Math.floor(Math.random() * charSet.length));
+  }
+  return password;
 }
 
-// To validate users choice of password
-if (isNaN(passwordLength) || passwordLength < 10 || passwordLength > 64) {
-  alert('Password length must be a number between 10 and 64!');
-  return '';
-}
-if (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecial) {
-  alert('You must select at least one character type!');
-  return '';
-}
-
-// Function to generate password with user input
+// Event listener for generate button
+document.getElementById('generate').addEventListener('click', function() {
+  const passwordField = document.getElementById('password');
+  passwordField.value = generatePassword();
+});
 
 
-// //Don't touch this
-// Get references to the #generate element
-var generateBtn = document.querySelector('#generate');
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector('#password');
-  passwordText.value = password;
-}
 
-// Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
